0dfb882f5f4a6dc330531b021a913797629f1739
Solanodecoin <solanocoin20041@gmail.com>
2024-01-18 13:27:54 +0100

    Ejercicio implantacion V1.1

---
+++ SESIONES/index.php
@@ -0,0 +1,13 @@
+<?php
+    session_start();
+
+    if (isset($_SESSION["visitas"])){
+        $_SESSION['visitas']++;
+        
+
+    }else{
+        $_SESSION['visitas'] = 1;
+    }
+echo $_SESSION['visitas'];
+
+?>
